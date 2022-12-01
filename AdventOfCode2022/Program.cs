80defdffca1a86be6dff13946bbe4eda51f452e0
foxt- <danig.dev@gmail.com>
2022-12-01 23:02:21 +0100

    day1 finished

---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode2022
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await new Day1().Run();
+        }
+    }
+}
